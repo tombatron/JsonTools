4fbf610279effd98b10c2e0f0972a53a80557065
Tommy Hanks <tommy.hanks@gmail.com>
2015-01-20 15:45:37 -0500

    Added more test cases.

--- src/test/java/com/tombatron/jsontools/IsTests.java
+++ src/test/java/com/tombatron/jsontools/IsTests.java
@@ -158,11 +158,6 @@ public class IsTests {
     }
 
     @Test
-    public void unescapedSolidusIsInvalid() {
-        assertFalse(Is.json("{\"solidus\":\"/\"}"));
-    }
-
-    @Test
     public void escapedSolidusIsValid() {
         assertTrue(Is.json("{\"solidus\":\"\\/\"}"));
     }
@@ -293,5 +288,76 @@ public class IsTests {
                 "    [1, 0, 0],\n" +
                 "    [0, 0, 1]\n" +
                 "]"));
+    }
+
+    @Test
+    public void testCase4IsValid() {
+        assertTrue(Is.json("{\n" +
+                "    \"glossary\": {\n" +
+                "        \"title\": \"example glossary\",\n" +
+                "\t\t\"GlossDiv\": {\n" +
+                "            \"title\": \"S\",\n" +
+                "\t\t\t\"GlossList\": {\n" +
+                "                \"GlossEntry\": {\n" +
+                "                    \"ID\": \"SGML\",\n" +
+                "\t\t\t\t\t\"SortAs\": \"SGML\",\n" +
+                "\t\t\t\t\t\"GlossTerm\": \"Standard Generalized Markup Language\",\n" +
+                "\t\t\t\t\t\"Acronym\": \"SGML\",\n" +
+                "\t\t\t\t\t\"Abbrev\": \"ISO 8879:1986\",\n" +
+                "\t\t\t\t\t\"GlossDef\": {\n" +
+                "                        \"para\": \"A meta-markup language, used to create markup languages such as DocBook.\",\n" +
+                "\t\t\t\t\t\t\"GlossSeeAlso\": [\"GML\", \"XML\"]\n" +
+                "                    },\n" +
+                "\t\t\t\t\t\"GlossSee\": \"markup\"\n" +
+                "                }\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }\n" +
+                "}"));
+    }
+
+    @Test
+    public void testCase5IsValid() {
+        assertTrue(Is.json("{\"menu\": {\n" +
+                "  \"id\": \"file\",\n" +
+                "  \"value\": \"File\",\n" +
+                "  \"popup\": {\n" +
+                "    \"menuitem\": [\n" +
+                "      {\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},\n" +
+                "      {\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},\n" +
+                "      {\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}\n" +
+                "    ]\n" +
+                "  }\n" +
+                "}}"));
+    }
+
+    @Test
+    public void testCase6IsValid() {
+        assertTrue(Is.json("{\"widget\": {\n" +
+                "    \"debug\": \"on\",\n" +
+                "    \"window\": {\n" +
+                "        \"title\": \"Sample Konfabulator Widget\",\n" +
+                "        \"name\": \"main_window\",\n" +
+                "        \"width\": 500,\n" +
+                "        \"height\": 500\n" +
+                "    },\n" +
+                "    \"image\": { \n" +
+                "        \"src\": \"Images/Sun.png\",\n" +
+                "        \"name\": \"sun1\",\n" +
+                "        \"hOffset\": 250,\n" +
+                "        \"vOffset\": 250,\n" +
+                "        \"alignment\": \"center\"\n" +
+                "    },\n" +
+                "    \"text\": {\n" +
+                "        \"data\": \"Click Here\",\n" +
+                "        \"size\": 36,\n" +
+                "        \"style\": \"bold\",\n" +
+                "        \"name\": \"text1\",\n" +
+                "        \"hOffset\": 250,\n" +
+                "        \"vOffset\": 100,\n" +
+                "        \"alignment\": \"center\",\n" +
+                "        \"onMouseUp\": \"sun1.opacity = (sun1.opacity / 100) * 90;\"\n" +
+                "    }\n" +
+                "}}"));
     }
 }
